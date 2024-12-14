9e7beaab95980af146ad8bc5526ca02224c2bece
laurynxrobson <laurynxrobson@gmail.com>
2024-12-14 15:50:00 +0200

    Changes a and img to Link and Image

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -8,13 +8,13 @@ export default function Footer() {
       <div className="mx-auto max-w-screen-xl">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a href="/" className="flex items-center">
-              <img
+            <Link href="/" className="flex items-center">
+              <Image
                 src={"/apple-touch-icon.png"}
                 className="mr-3 h-20"
                 alt={"Lauryn Robson logo"}
               />
-            </a>
+            </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
